32b89ec14024c2c9895522afe664812fea4c103e
Divij Ghose <divijghose@gmail.com>
2022-07-08 13:20:50 +0530

    MATLAB codes to test initialization

--- Test_Scripts/Octave_Tests/InitializationTest.m
+++ Test_Scripts/Octave_Tests/InitializationTest.m
@@ -1,4 +1,6 @@
+pkg load statistics
 Refinement = 6;
+N_R = 500;
 [xx,yy] = getMesh (Refinement);
 E = 0.03;
 Disp = 0.3;
@@ -6,4 +8,15 @@ Power = 2;
 LengthScale = 0.1;
 Cov = CovarianceKernel (xx,yy,Refinement,LengthScale,E,Disp,Power);
 [U,S,Vt] = svd(Cov);
+dS = diag(S);
+i=1;
+while(cumsum(dS)(i)/sum(dS)<0.999)
+  i++;
+endwhile
+Unew = U(:,1:i);
+SNew = dS(1:i);
+GaussD = stdnormal_rnd(i,N_R);
+Z = GaussD.*SNew;
+RealznVector = Unew*Z;
+
 
