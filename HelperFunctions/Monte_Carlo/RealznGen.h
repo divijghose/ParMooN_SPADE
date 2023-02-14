db19bcd49fd0e8ba2a0281f189f97fb9d6f9c8bd
Divij <divijghose@gmail.com>
2023-02-14 10:46:43 +0530

    Clean-up

--- HelperFunctions/Monte_Carlo/RealznGen.h
+++ HelperFunctions/Monte_Carlo/RealznGen.h
@@ -62,11 +62,199 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
             x[i] = double(1.0 / (N - 1)) * local_j;
             y[i] = double(1.0 / (N - 1)) * local_i;
         }
+        int kmax = 4;
+        int Nx, Ny;
+        Nx = sqrt(N_DOF);
+        Ny = sqrt(N_DOF);
+        double *wgt = new double[Nx * Ny]();
+        double *wgt_int = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_right = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_left = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_top = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_bottom = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_rt = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_rb = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_lt = new double[(Nx - 2) * (Ny - 2)]();
+        double *wgt_lb = new double[(Nx - 2) * (Ny - 2)]();
+        if (TDatabase::ParamDB->stddev_switch == 5)
+        {
+
+            for (int a = 0; a < N_DOF; a++)
+            {
+                if (1 - x[a] <= 1e-6 || 1 - y[a] <= 1e-6 || x[a] - 0 <= 1e-6 || y[a] - 0 <= 1e-6)
+                    wgt[a] = 0;
+                else
+                    wgt[a] = 1;
+            }
+
+            for (int k = 0; k < kmax; k++)
+            {
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt_int[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 1)];
+                        wgt_right[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 2)];
+                        wgt_left[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d)];
+                        wgt_top[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 1)];
+                        wgt_bottom[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 1)];
+                        wgt_rt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 2)];
+                        wgt_rb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 2)];
+                        wgt_lt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d)];
+                        wgt_lb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d)];
+                    }
+                }
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt[((b + 1) * Nx) + (d + 1)] *= (wgt_right[b * (Nx - 2) + d] + wgt_left[b * (Nx - 2) + d] + wgt_top[b * (Nx - 2) + d] + wgt_bottom[b * (Nx - 2) + d] + wgt_rt[b * (Nx - 2) + d] + wgt_rb[b * (Nx - 2) + d] + wgt_lt[b * (Nx - 2) + d] + wgt_lb[b * (Nx - 2) + d]) / 8;
+                    }
+                }
+            }
+            printToTxt("wgt.txt", wgt, Ny, Nx, 'R');
+        }
+        else if (TDatabase::ParamDB->stddev_switch == 6)
+        {
+            double disp = TDatabase::ParamDB->stddev_disp;
+            double halfwidth = 0.2;
+            double xcenter, ycenter;
+            xcenter = 0.0;
+            ycenter = 0.0;
+            double xdist, ydist, refxdist, refydist;
+            refxdist = 2.0;
+            refydist = 2.0;
+            for (int m = 0; m < N_DOF; m++)
+            {
+                xdist = fabs(x[m] - disp);
+                if (xdist <= refxdist)
+                {
+                    xcenter = x[m];
+                    refxdist = xdist;
+                }
+            }
+            for (int m = 0; m < N_DOF; m++)
+            {
+                ydist = fabs(y[m] - disp);
+                if (ydist <= refydist)
+                {
+                    ycenter = y[m];
+                    refydist = ydist;
+                }
+            }
+
+            int Nx, Ny;
+            Nx = sqrt(N_DOF);
+            Ny = sqrt(N_DOF);
+
+            for (int k = 0; k < N_DOF; k++)
+            {
+                if (fabs(x[k] - xcenter) <= halfwidth && fabs(y[k] - ycenter) <= halfwidth)
+                    wgt[k] = 1;
+            }
+
+            kmax = 6;
+            for (int k = 0; k < kmax; k++)
+            {
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt_int[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 1)];
+                        wgt_right[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 2)];
+                        wgt_left[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d)];
+                        wgt_top[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 1)];
+                        wgt_bottom[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 1)];
+                        wgt_rt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 2)];
+                        wgt_rb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 2)];
+                        wgt_lt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d)];
+                        wgt_lb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d)];
+                    }
+                }
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt[((b + 1) * Nx) + (d + 1)] *= (wgt_right[b * (Nx - 2) + d] + wgt_left[b * (Nx - 2) + d] + wgt_top[b * (Nx - 2) + d] + wgt_bottom[b * (Nx - 2) + d] + wgt_rt[b * (Nx - 2) + d] + wgt_rb[b * (Nx - 2) + d] + wgt_lt[b * (Nx - 2) + d] + wgt_lb[b * (Nx - 2) + d]) / 8;
+                    }
+                }
+            }
+            printToTxt("wgt.txt", wgt, Ny, Nx, 'R');
+        }
+        else if (TDatabase::ParamDB->stddev_switch == 7)
+        {
+            double disp = TDatabase::ParamDB->stddev_disp;
+            double halfwidth = 0.2;
+            double xcenter, ycenter;
+            xcenter = 0.0;
+            ycenter = 0.0;
+            double xdist, ydist, refxdist, refydist;
+            refxdist = 2.0;
+            refydist = 2.0;
+            for (int m = 0; m < N_DOF; m++)
+            {
+                xdist = fabs(x[m] - disp);
+                if (xdist <= refxdist)
+                {
+                    xcenter = x[m];
+                    refxdist = xdist;
+                }
+            }
+            for (int m = 0; m < N_DOF; m++)
+            {
+                ydist = fabs(y[m] - disp);
+                if (ydist <= refydist)
+                {
+                    ycenter = y[m];
+                    refydist = ydist;
+                }
+            }
+
+            int Nx, Ny;
+            Nx = sqrt(N_DOF);
+            Ny = sqrt(N_DOF);
+
+            for (int k = 0; k < N_DOF; k++)
+            {
+                if (sqrt(pow(x[k] - xcenter, 2) + pow(y[k] - ycenter, 2)) <= halfwidth)
+                    wgt[k] = 1;
+            }
+
+            kmax = 7;
+            for (int k = 0; k < kmax; k++)
+            {
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt_int[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 1)];
+                        wgt_right[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d + 2)];
+                        wgt_left[b * (Nx - 2) + d] = wgt[((b + 1) * Nx) + (d)];
+                        wgt_top[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 1)];
+                        wgt_bottom[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 1)];
+                        wgt_rt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d + 2)];
+                        wgt_rb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d + 2)];
+                        wgt_lt[b * (Nx - 2) + d] = wgt[((b)*Nx) + (d)];
+                        wgt_lb[b * (Nx - 2) + d] = wgt[((b + 2) * Nx) + (d)];
+                    }
+                }
+                for (int b = 0; b < Ny - 2; b++)
+                {
+                    for (int d = 0; d < Nx - 2; d++)
+                    {
+                        wgt[((b + 1) * Nx) + (d + 1)] *= (wgt_right[b * (Nx - 2) + d] + wgt_left[b * (Nx - 2) + d] + wgt_top[b * (Nx - 2) + d] + wgt_bottom[b * (Nx - 2) + d] + wgt_rt[b * (Nx - 2) + d] + wgt_rb[b * (Nx - 2) + d] + wgt_lt[b * (Nx - 2) + d] + wgt_lb[b * (Nx - 2) + d]) / 8;
+                    }
+                }
+            }
+            printToTxt("wgt.txt", wgt, Ny, Nx, 'R');
+        }
 
         double *C = new double[N_DOF * N_DOF]();  // MATRIX
         double *C1 = new double[N_DOF * N_DOF](); // MATRIX  - Corelation Matrix
 
+        double r = 0.0;
         double norm = 0;
+
         for (int i = 0; i < N_DOF; i++)
         {
             double actual_x = x[i];
@@ -77,11 +265,11 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
                 double local_x = x[j];
                 double local_y = y[j];
 
-                double r = sqrt(pow((actual_x - local_x), 2) + pow((actual_y - local_y), 2));
+                r = sqrt(pow((actual_x - local_x), 2) + pow((actual_y - local_y), 2));
 
                 // CO -Relation
-                C[i * N_DOF + j] = exp((-1.0 * r) / (LengthScale)) * (1 + (r / LengthScale) + pow(r / LengthScale, 2));
-                C1[i * N_DOF + j] = exp((-1.0 * r) / (LengthScale)) * (1 + (r / LengthScale) + pow(r / LengthScale, 2));
+                C[i * N_DOF + j] = exp((-1.0 * r * 10.0) / (LengthScale)) * (1.0 + (10.0 * r / LengthScale) + (pow(10.0 * r / (LengthScale), 2) / 3.0));
+                C1[i * N_DOF + j] = exp((-1.0 * r) / (LengthScale)) * (1 + (r / LengthScale) + pow(r / (LengthScale), 2) / 3.0);
 
                 if (TDatabase::ParamDB->stddev_switch == 0)
                 {
@@ -93,13 +281,14 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
 
                 else if (TDatabase::ParamDB->stddev_switch == 1)
                 {
+
                     double E = TDatabase::ParamDB->stddev_denom;
                     double disp = TDatabase::ParamDB->stddev_disp;
                     double power = TDatabase::ParamDB->stddev_power;
                     double sig_r1 = (exp(-1.0 * pow((2 * actual_x - 1 - disp), power) / (E)) / (2 * Pi * sqrt(E))) * (exp(-1.0 * pow((2 * actual_y - 1 - disp), power) / (E)) / (2 * Pi * sqrt(E)));
                     double sig_r2 = (exp(-1.0 * pow((2 * local_x - 1 - disp), power) / (E)) / (2 * Pi * sqrt(E))) * (exp(-1.0 * pow((2 * local_y - 1 - disp), power) / (E)) / (2 * Pi * sqrt(E)));
                     // Co Variance
-                    C[i * N_DOF + j] *= 0.5 * sig_r1 * sig_r2;
+                    C[i * N_DOF + j] *= 1.0 * sig_r1 * sig_r2;
                 }
 
                 else if (TDatabase::ParamDB->stddev_switch == 2)
@@ -110,6 +299,58 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
                     C[i * N_DOF + j] *= sig_r1 * sig_r2;
                 }
 
+                else if (TDatabase::ParamDB->stddev_switch == 3)
+                {
+
+                    double sigma = 1 / (LengthScale * (2.0 * sqrt(Pi)));
+                    double E = TDatabase::ParamDB->stddev_denom;
+                    double disp = TDatabase::ParamDB->stddev_disp;
+                    double power = TDatabase::ParamDB->stddev_power;
+                    double sig_r1 = sigma * (exp(-1.0 * pow((2 * actual_x - 1 - disp), power) / (E * pow(LengthScale, 2)))) * (exp(-1.0 * pow((2 * actual_y - 1 - disp), power) / (E * pow(LengthScale, 2))));
+                    double sig_r2 = sigma * (exp(-1.0 * pow((2 * local_x - 1 - disp), power) / (E * pow(LengthScale, 2)))) * (exp(-1.0 * pow((2 * local_y - 1 - disp), power) / (E * pow(LengthScale, 2))));
+                    // Co Variance
+                    C[i * N_DOF + j] *= 1 * sig_r1 * sig_r2;
+                }
+                else if (TDatabase::ParamDB->stddev_switch == 4)
+                {
+
+                    double height = 1;
+                    double disp = TDatabase::ParamDB->stddev_disp;
+                    double spread = TDatabase::ParamDB->stddev_denom;
+                    double power = TDatabase::ParamDB->stddev_power;
+                    double sig_r1 = height * (exp(-1.0 * (((pow(2.0 * actual_x - 1.0 - disp, power)) / (pow(spread, 2))))) * (exp(-1.0 * ((pow(2.0 * actual_y - 1.0 - disp, power)) / (pow(spread, 2))))));
+                    double sig_r2 = height * (exp(-1.0 * (((pow(2.0 * local_x - 1.0 - disp, power)) / (pow(spread, 2))))) * (exp(-1.0 * ((pow(2 * local_y - 1.0 - disp, power)) / (pow(spread, 2))))));
+                    // Co Variance
+                    C[i * N_DOF + j] *= sig_r1 * sig_r2 * 5e-3;
+                }
+                else if (TDatabase::ParamDB->stddev_switch == 5)
+                {
+
+                    double sig_r1, sig_r2;
+                    sig_r1 = wgt[i];
+                    sig_r2 = wgt[j];
+                    // Co Variance
+                    // C[i * N_DOF + j] *= sig_r1 * sig_r2;
+                    C[i * N_DOF + j] *= sig_r1 * sig_r2;
+                }
+                else if (TDatabase::ParamDB->stddev_switch == 6)
+                {
+                    double sig_r1, sig_r2;
+                    sig_r1 = wgt[i];
+                    sig_r2 = wgt[j];
+                    // Co Variance
+                    // C[i * N_DOF + j] *= sig_r1 * sig_r2;
+                    C[i * N_DOF + j] *= sig_r1 * sig_r2;
+                }
+                else if (TDatabase::ParamDB->stddev_switch == 7)
+                {
+                    double sig_r1, sig_r2;
+                    sig_r1 = wgt[i];
+                    sig_r2 = wgt[j];
+                    // Co Variance
+                    // C[i * N_DOF + j] *= sig_r1 * sig_r2;
+                    C[i * N_DOF + j] *= sig_r1 * sig_r2*700;
+                }
                 else
                 {
                     cout << "Error - No standard deviation function is defined for stddev_switch: " << TDatabase::ParamDB->stddev_switch << endl;
@@ -127,10 +368,14 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
         // Declare SVD parameters
         MKL_INT m1 = N_DOF, n = N_DOF, lda = N_DOF, ldu = N_DOF, ldvt = N_DOF, info;
         double superb[std::min(N_DOF, N_DOF) - 1];
+        double w[N];
 
         double *S = new double[N_DOF]();
         double *U = new double[N_DOF * N_DOF]();
         double *Vt = new double[N_DOF * N_DOF]();
+
+        // info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', N_DOF, C, N_DOF, S);
+
         info = LAPACKE_dgesvd(LAPACK_ROW_MAJOR, 'A', 'A', m1, n, C, lda,
                               S, U, ldu, Vt, ldvt, superb);
 
@@ -195,17 +440,21 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
             }
         }
 
+        double varNormFactor = S[0];
+        // for (int k = 0; k < modDim; k++)
+        //     S[k] = (S[k] / varNormFactor);
+
         for (int k = 0; k < modDim; k++)
         {
             std::random_device rd{};
             std::mt19937 gen{rd()};
-            std::normal_distribution<> d{0, 1};
+            std::normal_distribution<> d{0, sqrt(S[k])};
 
             double *norm1 = new double[N_Realisations]();
 
             for (int n = 0; n < N_Realisations; ++n)
             {
-                Z[k * N_Realisations + n] = S[k] * d(gen);
+                Z[k * N_Realisations + n] = d(gen);
             }
         }
 
@@ -251,7 +500,5 @@ void GenerateRealizations(TFESpace2D *Scalar_FeSpace, double *RealizationVector)
         exit(0);
     }
 
-
     /////////////////////////////////////// -------- END OF REALISATION DATA SETS ------------ ////////////////////////////////////////////////////////////////
-
 }
